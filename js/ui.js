8cf13e8997cddbbaf168c4986ed24661b0e78d6e
Rogerio Emmanuel Canto Romero <rogerio_canto@MacBook-Air-de-Rogerio.local>
2026-07-18 13:58:35 -0600

    Add showEPIC function and grid styles

--- js/ui.js
+++ js/ui.js
@@ -19,4 +19,33 @@ function showAPOD(data) {
 
 }
 
-getAPOD().then(data => showAPOD(data))
+function showEPIC(data) {
+    
+    mainContent.innerHTML = '<h2>Earth from Space</h2>'
+    
+    const grid = document.createElement('div')
+    grid.className = 'epic-grid'
+
+    const images = data.slice(0, 12)
+
+    images.map( img => {
+        const card = document.createElement('div')
+        card.className = 'epic-card'
+
+        const date = img.date.split(' ')[0] // "2026-07-16"
+        const datePath = date.split('-').join('/') // "2026/07/16"
+        const imageUrl = `https://api.nasa.gov/EPIC/archive/natural/${datePath}/png/${img.image}.png?api_key=${CONFIG.API_KEY}`
+       
+        card.innerHTML = `
+                <img src="${imageUrl}" alt="image" />
+                <p>${img.date}</p>
+        `
+
+        grid.appendChild(card)
+    })
+
+    mainContent.appendChild(grid)
+
+}
+
+getEPIC().then(data => showEPIC(data))
